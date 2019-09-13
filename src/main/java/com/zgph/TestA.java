27fb0596d6b269d4d376b55930cfdd103981e66d
lwg <15983316702@163.com>
2019-09-13 09:34:22 +0800

    初始化

---
+++ src/main/java/com/zgph/TestA.java
@@ -0,0 +1,21 @@
+package com.zgph;
+
+/**
+ * @author lwg
+ * @Date: 2019/9/13 9:31
+ */
+public class TestA {
+    public static void main(String[] args) {
+
+        System.out.println("hello-java!!!");
+        
+
+        int a = 10;
+        if(a>=9){
+            System.out.println("a>=9");
+        }else {
+            System.out.println("a<=9");
+        }
+
+    }
+}
